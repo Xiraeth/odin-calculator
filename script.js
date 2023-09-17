31d1f1ca11e9abfa6344e8d57983b3cc60286599
Xiraeth <77112995+Xiraeth@users.noreply.github.com>
2023-09-17 15:10:33 +0300

    Create starter files for the app
    Create functions in the index.js file and the appearance of
    the calculator

---
+++ script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const buttons = document.querySelectorAll("button");
+const container = document.querySelector(".calculator");
+console.log(container);
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(num1, operator, num2) {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      return num1 / num2;
+  }
+}
