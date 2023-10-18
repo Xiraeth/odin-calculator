b50c42d5ab4fd2adef82173e6a6219ed25bf19f5
Xiraeth <77112995+Xiraeth@users.noreply.github.com>
2023-10-19 00:21:07 +0300

    make operator button display result

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const container = document.querySelector(".container");
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".ops");
 const result = document.querySelector(".result");
 const operand = document.querySelector(".operand");
 const clearButton = document.querySelector(".clear");
@@ -42,14 +43,21 @@ class Calculator {
   };
 
   numberPress(btn) {
-    if (!btn) return;
-
     if (this.operand.includes(".") && btn.textContent == ".") return;
     if (this.operand == "0") this.operand = btn.textContent;
     else this.operand += btn.textContent;
   }
 
-  operatorButtonPress() {}
+  operatorButtonPress(op) {
+    operator = op.textContent;
+    if (this.operand == "") return;
+    if (this.result == 0) {
+      this.result = this.operand;
+      this.operand = "";
+      return;
+    }
+    this.operand = "";
+  }
 
   delete() {}
 
@@ -58,9 +66,18 @@ class Calculator {
 
 const calc = new Calculator(operand.textContent, operator, result.textContent);
 
-container.addEventListener("click", (e) => {
-  calc.numberPress(e.target.closest(".number"));
-  calc.updateDisplay();
+numberButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    calc.numberPress(e.target);
+    calc.updateDisplay();
+  });
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    calc.operatorButtonPress(e.target);
+    calc.updateDisplay();
+  });
 });
 
 clearButton.addEventListener("click", calc.clear);
