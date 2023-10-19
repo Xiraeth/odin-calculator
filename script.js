c013069156d4dbfcbf038a005de87a2eceb6b763
Nick Iliadis <77112995+Xiraeth@users.noreply.github.com>
2023-10-19 13:23:35 +0300

    Replace script
    Make 'operator' and 'clear' buttons fully functional  
    Re-upload file because remote work

---
+++ script.js
@@ -0,0 +1,107 @@
+"use strict";
+
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".ops");
+const result = document.querySelector(".result");
+const operand = document.querySelector(".operand");
+const clearButton = document.querySelector(".clear");
+const equalsButton = document.querySelector(".equals");
+const deleteButton = document.querySelector(".delete");
+let operator;
+
+class Calculator {
+  constructor(operand, operator, result) {
+    this.operand = operand;
+    this.operator = operator;
+    this.result = result;
+    this.clear();
+  }
+
+  operate(a, op, b) {
+    switch (op) {
+      case "+":
+        return a + b;
+      case "-":
+        return a - b;
+      case "×":
+        return a * b;
+      case "÷":
+        return a / b;
+    }
+  }
+
+  updateDisplay() {
+	if(!operator) {
+		result.textContent = this.result;
+		operand.textContent = this.operand;
+		return;
+	}
+    result.textContent = this.result + operator;
+    operand.textContent = this.operand;
+  }
+
+  clear = () => {
+    this.result = "0";
+    this.operand = "0";
+	operator = undefined;
+    console.clear();
+    this.updateDisplay();
+  };
+
+  numberPress(btn) {
+    if (this.operand.includes(".") && btn.textContent == ".") return;
+    if (this.operand == "0") this.operand = btn.textContent;
+    else this.operand += btn.textContent;
+  }
+
+  operatorButtonPress(op) {
+    if (this.operand == "") {
+		operator = op.textContent;
+		return;
+	};
+    if (this.result == 0) {
+      this.result = this.operand;
+	  operator = op.textContent;
+      this.operand = "";
+      return;
+    }
+	
+	this.result = this.operate(Number(this.result), 
+	operator, Number(this.operand));
+	
+    this.operand = "";
+	operator = op.textContent;
+  }
+
+  delete() {}
+
+  equals() {
+	  if(this.operand == '') return;
+	  this.result = this.operate(Number(this.result), 
+	  operator, Number(this.operand));
+	  this.operand = '';
+  }
+}
+
+const calc = new Calculator(operand.textContent, operator, result.textContent);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    calc.numberPress(e.target);
+    calc.updateDisplay();
+  });
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    calc.operatorButtonPress(e.target);
+    calc.updateDisplay(e.target);
+  });
+});
+
+equalsButton.addEventListener('click', e => {
+	calc.equals();
+	calc.updateDisplay();
+})
+
+clearButton.addEventListener("click", calc.clear);
